a319d63267d0d47957fa25a47416db4b669c0398
sbagroy986 <shrey14099@iiitd.ac.in>
2016-11-11 16:52:58 +0530

    Adding KFold split

--- Code/classifier.py
+++ Code/classifier.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import KFold
 import os
 from os import listdir
 from os.path import isfile, join
@@ -9,25 +10,37 @@ def get_hog(image):
 	return np.array(i.histogram())
 
 
-def get_files():
-	directory=os.getcwd()+"/training/negative/"
-	files = [f for f in listdir(directory) if isfile(join(directory, f))]
-	neg=[]
-	for file in files:
-		if "jpg" not in file and "png" not in file:
-			continue
-		neg.append(file)
-
-
+def get_data():
 	directory=os.getcwd()+"/training/positive_expanded/"
 	files = [f for f in listdir(directory) if isfile(join(directory, f))]
 	pos=[]
 	for file in files:
 		if "jpg" not in file and "png" not in file:
 			continue
-		pos.append(file)
+		pos.append(get_hog(file))
+
+	directory=os.getcwd()+"/training/negative/"
+	files = [f for f in listdir(directory) if isfile(join(directory, f))]
+	neg=[]
+	for file in files:
+		if "jpg" not in file and "png" not in file:
+			continue
+		neg.append(get_hog(file))
+	shuffle(neg)
+	neg=neg[:len(pos)]
 	return pos,neg
 
-pos,neg=get_files()
+def kfold_split(positive,negative,k=5):
+	y_pos=[]
+	y_neg=[]
+	y=[]
+	for i in positive:
+		y_pos.append(1)
+	for i in negative:
+		y_neg.append(0)
+
+	
+pos,neg=get_data()
 print "Number of positive samples: ",len(pos)
 print "Number of negative samples: ",len(neg)
+
